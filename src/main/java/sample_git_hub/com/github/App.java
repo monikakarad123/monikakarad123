367cbbb5fc55725b62794e545f1a9a103e9f4642
monikakarad123 <monikakarad999@gmail.com>
2022-12-13 13:14:07 +0530

    first commit

---
+++ src/main/java/sample_git_hub/com/github/App.java
@@ -0,0 +1,13 @@
+package sample_git_hub.com.github;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/sample_git_hub/com/github/AppTest.java
@@ -0,0 +1,20 @@
+package sample_git_hub.com.github;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
